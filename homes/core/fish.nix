6984b494afd12db0ceef2ce1b9364e86870e5ab5
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-11-18 17:16:58 +0300

    Removed some packages

--- homes/core/fish.nix
+++ homes/core/fish.nix
@@ -9,67 +9,11 @@ in
   with pkgs; {
     home.packages = [
       bat
-      commitizen
       fzf
-      zoxide
     ];
 
     programs.fish = {
       enable = true;
-
-      functions = {
-        gitignore = "curl -sL https://www.gitignore.io/api/$argv";
-        fish_greeting = ""; # disable welcome text
-        bs = ''
-          pushd ~/.config/nixos
-          nh os switch .
-            if test $status -eq 0
-              notify-send "Rebuild Switch" "Build successful!"
-            else
-              notify-send "Rebuild Switch" "Build failed!"
-            end
-          popd
-        '';
-        bb = ''
-          pushd ~/.config/nixos
-          nh os boot .
-            if test $status -eq 0
-              notify-send "Rebuild Boot" "Build successful!"
-            else
-              notify-send "Rebuild Boot" "Build failed!"
-            end
-          popd
-        '';
-        fe = ''
-          set selected_file (rg --files ''$argv[1] | fzf --preview "bat -f {}")
-
-          if [ -n "$selected_file" ]
-              echo "$selected_file" | xargs $EDITOR
-          end
-        '';
-        prj = ''
-          # Credit to https://www.reddit.com/r/fishshell/comments/152zkrz/
-          if not command -q fzf
-                  echo >&2 "prj: fzf command not found. Install with your OS package manager."
-                  return 1
-              end
-
-          # determine the project home
-          set -q MY_PROJECTS || set -l MY_PROJECTS ~/Code
-          set -l prjfolders (path dirname $MY_PROJECTS/**/.git)
-
-          # use fzf to navigate to a project
-          set -l prjlist (string replace $MY_PROJECTS/ "" $prjfolders)
-          set -l selection (printf '%s\n' $prjlist | sort | fzf --layout=reverse-list --query="$argv")
-          test $status -eq 0 || return $status
-          echo "Navigating to '$selection'."
-          cd $MY_PROJECTS/$selection
-        '';
-        run = "nix run nixpkgs#$argv[1] -- $argv[2..-1]";
-        "watchLive" = let
-          args = "--hwdec=dxva2 --gpu-context=d3d11 --no-keepaspect-window --keep-open=no --force-window=yes --force-seekable=yes --hr-seek=yes --hr-seek-framedrop=yes";
-        in "${_ streamlink} --player ${_ mpv} --twitch-disable-hosting --twitch-low-latency --player-args \"${args}\" --player-continuous-http --player-no-close --hls-live-edge 2 --stream-segment-threads 2 --retry-open 15 --retry-streams 15 $argv best -a --ontop -a --no-border";
-      };
 
       interactiveShellInit = ''
         set -g async_prompt_functions _pure_prompt_git
@@ -78,7 +22,6 @@ in
       '';
 
       shellAliases = {
-        "cd" = "${_ z}"
         "cat" = "${_ bat}";
         "nb" = "nix-build -E \'with import <nixpkgs> { }; callPackage ./default.nix { }\'";
         "mkdir" = "mkdir -p";
