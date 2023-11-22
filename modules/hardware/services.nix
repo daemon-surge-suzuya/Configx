9a3b21b978862a6550ab18b8004db18bc8f80215
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-11-22 11:21:45 +0300

    Added services in hardware

---
+++ modules/hardware/services.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.libinput.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbVariant = "";
+
+}
