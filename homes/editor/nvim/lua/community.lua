bb4c9dbc7a5aab7acd3b1126e4a73920648e1842
daemon-surge-suzuya <daemonsurges@gmail.com>
2024-10-13 14:12:20 +0530

    fix

--- homes/editor/nvim/lua/community.lua
+++ homes/editor/nvim/lua/community.lua
@@ -1,8 +1,9 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.git/git-blame-nvim", enabled = true }, 
-  { import = "astrocommunity.utility/noice-nvim", enabled = true },
-  { import = "astrocommunity.colorscheme.rose-pine", enabled = true },
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.git.git-blame-nvim", enabled = true },
+  { import = "astrocommunity.utility.noice-nvim", enabled = true },
+  -- { import = "astrocommunity.colorscheme.rose-pine", enabled = true },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
+
