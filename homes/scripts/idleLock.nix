b1ab1dbdfdf44c7c14e8a79b523f0dfd66e0ed2a
daemon-surge-suzuya <daemonsurges@gmail.com>
2024-11-11 20:00:14 +0530

    cleaned firefox

--- homes/scripts/idleLock.nix
+++ homes/scripts/idleLock.nix
@@ -6,7 +6,7 @@ in
 
 pkgs.writeShellScriptBin "idleLock" ''
 
-IDLE_THRESHOLD=10
+IDLE_THRESHOLD=2
 COMMAND="i3lock"
 
 if [ "$#" -gt 0 ]; then
@@ -21,13 +21,18 @@ IDLE_THRESHOLD=$((IDLE_THRESHOLD * 1000))
 while true; do
     IDLE_TIME=$(${_ pkgs.xprintidle})
 
+    MEDIA_STATUS=$(${_ pkgs.playerctl} --all-players status 2>/dev/null)
+
     if [ "$IDLE_TIME" -gt "$IDLE_THRESHOLD" ]; then
-        if ! pgrep -x "$(basename "$COMMAND")" > /dev/null; then
-            eval "$COMMAND"
+        if [[ "$MEDIA_STATUS" != "Playing" ]]; then
+            if ! pgrep -x "$(basename "$COMMAND")" > /dev/null; then
+                eval "$COMMAND"
+            fi
         fi
     fi
 
     sleep 1
 done
+
 
 ''
